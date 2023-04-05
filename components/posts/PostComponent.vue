35c220f9191767dddb59342ff2fe1f2950256f5f
Vova Rodin <mynameisvova@yahoo.com>
2023-04-05 13:55:00 +0300

    date format component

--- components/posts/PostComponent.vue
+++ components/posts/PostComponent.vue
@@ -4,8 +4,8 @@
       {{ post?.title }}
     </h2>
     <div class="mt-4" v-html="post?.content" />
-    <p class="mt-3 text-sm text-gray-500">
-      {{ post?.updatedAt }}
+    <p class="mt-3 text-xs text-gray-500">
+      <DateFormat :src="post?.updatedAt" />
     </p>
   </div>
 </template>
@@ -13,6 +13,7 @@
 <script lang="ts" setup>
 import { UnwrapRef } from 'vue'
 import { PostEntity } from '~/generated/schema'
+import DateFormat from '~/components/utilities/DateFormat.vue'
 
 defineProps<{
   postId?: string
---
+++ components/utilities/DateFormat.vue
@@ -0,0 +1,22 @@
+<template>
+  <time :datetime="datetime">
+    {{ date }}
+  </time>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs'
+
+const props = withDefaults(
+  defineProps<{
+    src: string
+    format?: string
+  }>(),
+  {
+    format: 'DD.MM.YYYY',
+  },
+)
+
+const datetime = computed(() => dayjs(props.src).format('YYYY-MM-DD'))
+const date = computed(() => dayjs(props.src).format(props.format))
+</script>
