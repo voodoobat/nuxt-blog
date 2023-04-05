2aec6c9276d96caadff9f94700f8d3969a153343
Vova Rodin <mynameisvova@yahoo.com>
2023-04-05 11:58:37 +0300

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,21 @@
 <template>
-  <nuxt-page />
+  <header class="shadow">
+    <LayoutContainer>
+      <div class="flex justify-between items-center py-4">
+        <nuxt-link to="/" class="text-2xl">
+          Strapi & nuxt
+        </nuxt-link>
+      </div>
+    </LayoutContainer>
+  </header>
+  <main class="grow">
+    <nuxt-page />
+  </main>
+  <footer>
+    <LayoutContainer>
+      <div class="text-center py-2 text-gray-500">
+        <p class="text-sm">Copyright © 2023 - Strapi & nuxt</p>
+      </div>
+    </LayoutContainer>
+  </footer>
 </template>
